21b325fd761b070dc1764ec55ff2bd18d81c4e8d
prathum08 <prathumbhangadia08@gmail.com>
2024-02-21 11:15:51 +0530

    This Program is for the sum of two number using the (cin) operation

---
+++ SumOfTwoNum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    cout << "Enter the First Number :- " ;
+    cin >> a;
+    
+    cout << "Enter the Second Number :- " ;
+    cin >> b;
+
+    int sum = a + b;
+    cout << "The Sum of " << a << " and " << b << " is :- " << sum << endl;
+
+}
