1a4663b8317a4dd7e1afbe45cd5892533cce49f9
prathum08 <prathumbhangadia08@gmail.com>
2024-02-21 11:22:12 +0530

    This Program Find if the number is even or odd

---
+++ OddorEven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter the number :- ";
+    cin >> num;
+
+    if(num%2 == 0){
+        cout << num <<" is Even Number " << endl;
+    }
+    else{
+        cout << num << " is Odd Number " << endl;
+    }
+
+    return 0;
+}
